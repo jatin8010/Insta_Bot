58593e16ad9b61456cfb0bb8b593a47956eed3c3
jatin8010 <jatin80104013@gmail.com>
2017-07-04 23:26:23 +0530

    file having media infor

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import requests
+import requests , urllib
 
 APP_ACCESS_TOKEN = '2461812470.8614bda.3e2cc92411da4c0a93123df11cf4d922'
 
@@ -57,6 +57,43 @@ def get_user_info(insta_username):
     else:
         print 'Status code other than 200 received!'
 
+def get_own_post():
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    own_media = requests.get(request_url).json()
+    if own_media['meta']['code'] == 200:
+        if len(own_media['data']):
+         image_name = own_media['data'][0]['id'] + '.jpeg'
+         image_url = own_media['data'][0]['images']['standard_resolution']['url']
+         urllib.urlretrieve(image_url, image_name)
+         print 'Your image has been downloaded!'
+        else:
+           print 'Post does not exist!'
+    else:
+     print 'Status code other than 200 received!'
+
+
+    def get_user_post(insta_username):
+         user_id = get_user_id(insta_username)
+         if user_id == None:
+            print 'User does not exist!'
+            exit()
+         request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+         print 'GET request url : %s' % (request_url)
+         user_media = requests.get(request_url).json()
+
+         if user_media['meta']['code'] == 200:
+            if len(user_media['data']):
+                image_name = user_media['data'][0]['id'] + '.jpeg'
+                image_url = user_media['data'][0]['images']['standard_resolution']['url']
+                urllib.urlretrieve(image_url, image_name)
+                print 'Your image has been downloaded!'
+            else:
+                print 'Post does not exist!'
+         else:
+            print 'Status code other than 200 received!'
+
+
 def start_bot():
     while True:
         print '\n'
@@ -64,6 +101,8 @@ def start_bot():
         print 'Here are your menu options:'
         print "a.Get your own details\n"
         print "b.Get details of another user by username\n"
+        print "c.Get your own recent post\n"
+        print "d.Get the recent post of a user by username\n"
         print "z.Exit"
 
         choice=raw_input("Enter you choice: ")
@@ -72,11 +111,17 @@ def start_bot():
         elif choice=="b":
             insta_username = raw_input("Enter the username: ")
             get_user_info(insta_username)
+        elif choice == "c":
+            get_own_post()
+        elif choice == "d":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_post(insta_username)
         elif choice=="z":
             exit()
         else:
             print "incorrect choice"
 
 start_bot()
+
 
 
